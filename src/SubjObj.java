19ebc691bae9aec98e4295b06ac3ef000ecca712
shockolavawave <atharv.sambarat@gmail.com>
2023-02-23 12:39:28 +0530

    ; taking input from user - handling exceptions

--- src/SubjObj.java
+++ src/SubjObj.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class SubjObj {
+
+    public static Scanner scObj = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        try {
 
+            System.out.println("Enter the number of animals: ");
+            int size = scObj.nextInt();
+            System.out.println("Generating " + size + " animals...");
+
+        } catch (OutOfMemoryError e) {
+            System.out.println("Memory not enough.\nAborting program.");
+            System.exit(0);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input!\nAborting program.");
+            System.exit(0);
+        }
 
     }
 }

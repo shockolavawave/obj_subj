a28cfbd621e5e99b8a3846bc6b099783074a310c
shockolavawave <atharv.sambarat@gmail.com>
2023-02-23 12:48:05 +0530

    ; creating classes 'Both' and 'ObjOnly'

---
+++ src/Both.java
@@ -0,0 +1,16 @@
+public class Both {
+
+    private boolean O;
+    private boolean S;
+    private char attrib;
+
+    public boolean isO() {  return O;   }
+    public void setO(boolean o) {   O = o;  }
+
+    public boolean isS() {  return S;   }
+    public void setS(boolean s) {   S = s;  }
+
+    public char getAttrib() {   return attrib;  }
+    public void setAttrib(char attrib) {    this.attrib = attrib;   }
+
+}
---
+++ src/ObjOnly.java
@@ -0,0 +1,12 @@
+public class ObjOnly {
+
+    private boolean R;
+    private char attrib;
+
+    public boolean isR() {  return R;   }
+    public void setR(boolean r) {   R = r;  }
+
+    public char getAttrib() {   return attrib;  }
+    public void setAttrib(char attrib) {    this.attrib = attrib;   }
+
+}

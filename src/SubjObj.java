f70681264c08cf3627a694141614a1c3aa101e95
shockolavawave <atharv.sambarat@gmail.com>
2023-02-23 13:31:34 +0530

    ; completing countAndWrite() for Both[]

--- src/SubjObj.java
+++ src/SubjObj.java
@@ -12,6 +12,56 @@ public class SubjObj {
 
     // function for Both
     public static void countAndWrite(Both[] arr) {
+
+        // allocating random boolean to O and S
+        for (Both x :
+                arr) {
+            x.setO(rn.nextBoolean());
+            x.setS(rn.nextBoolean());
+        }
+
+        // calculating and assigning attrib
+        for (Both x:
+             arr) {
+
+            if ( (x.isO()) && (x.isS()) )
+                x.setAttrib('a');
+            else if ( (x.isO()) && !(x.isS()) )
+                x.setAttrib('b');
+            else if ( !(x.isO()) && (x.isS()) )
+                x.setAttrib('c');
+            else if ( !(x.isO()) && !(x.isS()) )
+                x.setAttrib('d');
+        }
+
+        // counting attrib
+        int a = 0, b = 0, c = 0, d = 0;
+        for (Both x :
+                arr) {
+            switch (x.getAttrib()) {
+                case 'a' -> a++;
+                case 'b' -> b++;
+                case 'c' -> c++;
+                case 'd' -> d++;
+            }
+        }
+
+        // writing csv file
+        try (FileWriter fw = new FileWriter("files/" + arr.length + "_Both.csv")) {
+
+            fw.write(
+                    "Selection, Consideration, Counts, Percentege\n" +
+                            "Positive, Subjective and Objective," + df.format(a) + "," + df.format((a/ arr.length) * 100L) + "%\n" +
+                            "Positive, Only Objective,"           + df.format(b) + "," + df.format((b/ arr.length) * 100L) + "%\n" +
+                            "Positive, Only Subjective,"          + df.format(c) + "," + df.format((c/ arr.length) * 100L) + "%\n" +
+                            "Negative, disqualified,"             + df.format(d) + "," + df.format((d/ arr.length) * 100L) + "%\n" +
+                            "-, Total,"                           + df.format(arr.length) + "," + df.format(100) + "%\n"
+            );
+
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+
     }
 
     // function for ObjOnly
